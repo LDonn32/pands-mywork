02926045ad46f017a3f546ba37d0baacb7fb31ac
Laura Donnelly <lauradonnelly32@gmail.com>
2025-02-20 21:19:30 +0000

    lab 3.1

---
+++ helloWorld.py
@@ -0,0 +1,5 @@
+ # helloWorld.py
+# This program just prints out hello World
+# Author: Laura Donnelly
+
+print ("hello World!")
---
+++ messingwithtypes.py
@@ -0,0 +1,9 @@
+# messing with varaible types
+# Author: Laura Donnelly
+# lab 3.1 
+
+answer_of_question = 12
+print(f"answer is {answer_of_question}")
+print("answer is " + str(answer_of_question))
+print(f"type of answer is {type(answer_of_question)}")
+print(f"type of type {type(type(answer_of_question))")
