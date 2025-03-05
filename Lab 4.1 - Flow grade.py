8486995b2cc9441db8b8fca76fe0278c07601ac9
Laura Donnelly <lauradonnelly32@gmail.com>
2025-03-05 20:25:12 +0000

    extra q in lab

--- Lab 4.1 - Flow grade.py
+++ Lab 4.1 - Flow grade.py
@@ -21,3 +21,12 @@ elif percentage < 70: # between 60 and 69
     print ("Merit2")
 else: # the only option left is between 70 and 100
     print ("Distinction")
+
+# Extra (I will not give the answer to these) 
+# In practice the percentages are rounded ie 69.5 gets rounded to 70 so is equal to a Distinction.  
+# How would you modify the program in 1 to deal with this? 
+
+
+
+
+# How would you use a while loop to modify 1 so that it keeps prompting the user for a number until the user enters -1
