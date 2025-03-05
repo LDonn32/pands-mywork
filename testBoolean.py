a8bd3ed62632f29e77d0e15585aa5870d74c9082
Laura Donnelly <lauradonnelly32@gmail.com>
2025-03-05 18:07:58 +0000

    lab 4.1 if else and elif

---
+++ testBoolean.py
@@ -0,0 +1,13 @@
+# messing with booleans
+
+#Author : laura donnelly
+is_alive = True
+print(f"is alive = {is_alive}")
+
+var = (2 <= 4)
+print(f"var is {var}")
+
+grade = 400
+invalid = (grade <= 0) or (grade >= 100)
+print(f"invalid is {invalid}")
+
