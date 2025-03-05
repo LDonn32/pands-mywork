7c0ff8236501ed05612067c5d941966130bf1b84
Laura Donnelly <lauradonnelly32@gmail.com>
2025-03-05 20:47:10 +0000

    lab 4.2

---
+++ Lab 4.2 - Flow testFor.py
@@ -0,0 +1,20 @@
+# Messing with for loops
+#Author: laura donnelly
+
+# for elem in list:
+    # do something
+
+# for number in range(1,10)
+    # do something
+boats = ["Sigma", "x yacht", "Swan"]
+
+for boat in boats:
+    print("i'd rather be out in a ", boat) 
+
+for i in range(1,10): # (1,10) is not inclusive of 10.
+    print (i)
+
+print("all done i is now", i)
+
+if "Swan" in boats:
+    print("that is a boat")
