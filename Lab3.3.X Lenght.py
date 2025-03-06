dba71430f3b575a63f8015d451531990008f42a6
Laura Donnelly <lauradonnelly32@gmail.com>
2025-03-06 10:47:56 +0000

    lab 3.3

---
+++ Lab3.3.X Lenght.py
@@ -0,0 +1,13 @@
+# This program reads in a String and outputs its lenght
+# Author: Laura Donnelly
+
+inputString = input("enter a string:")
+lenghtOfString = len(inputString)
+print(f"The lenght of {inputString} is {lenghtOfString }")
+
+
+# simple program creating and outputting a string
+# Author: Laura Donnelly
+
+message = 'John said\t"hi"\nI said\t\t"bye"'
+print (message)
