fa20a0415b1802636c6e310a0f9c6863a2965866
LDonn32 <lauradonnelly32@gmail.com>
2025-03-14 17:10:39 +0000

    lab 5.3

---
+++ messingWithDict.py
@@ -0,0 +1,14 @@
+# Messing with dictionaries
+# Author: Laura Donnelly
+
+car = {
+    "make" : "kia",
+    "model" : "picanto",
+    "year" : 2016,
+    "owner" : {
+        "name" : "Laura",
+    "driver-number" : 1123
+}
+}
+print(car["year"])
+print (car["owner"]("name"))
