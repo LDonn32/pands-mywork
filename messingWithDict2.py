0bff15dc1badc879399952c1a8ec1f730b6f1bd4
LDonn32 <lauradonnelly32@gmail.com>
2025-03-14 17:22:44 +0000

    lab 5.3

---
+++ messingWithDict2.py
@@ -0,0 +1,11 @@
+
+
+car = {
+    "make" : "Kia",
+    "model" : "niro",
+    "price" : "10000",
+    "tags" : ["pre-owned", "Best Buy", "Dealer"]
+}
+
+for key, value in car.items():
+    print(key,'has a value', value)
