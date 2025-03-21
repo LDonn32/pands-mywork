23d5b2e32e50f5ec4442e9b60f3e9d9108d815bb
LDonn32 <lauradonnelly32@gmail.com>
2025-03-21 13:39:52 +0000

    lab 6.1

--- Lab 6.1 Functions lecture.py
+++ Lab 6.1 Functions lecture.py
@@ -1,3 +1,17 @@
 # Lab 6.1 Functions Syntax
+# Messing with functions
 # Author: Laura Donnelly
+
+
+
+
+def topower(number, power=3):
+    #print(number)
+    return (number ** power)
+
+#ans = cube(23)
+#print(f"we got {ans}")
+
+num = 45
+print(f"and {num} is {topower(num,3)}")
 
