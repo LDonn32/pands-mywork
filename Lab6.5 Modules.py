f82121879a41cd131e5a3f61ed526870c6f781e0
LDonn32 <lauradonnelly32@gmail.com>
2025-03-26 20:13:39 +0000

    lab 6.5 mdoules

---
+++ Lab6.5 Modules.py
@@ -0,0 +1,23 @@
+# Messing with modules
+
+# Built in Modules
+# how to import modules 
+# From x import y
+# Import X as diffX
+# what is pip install
+
+# Making own modules
+# used for functions
+# for configuring files
+# path for imports
+
+
+from constraint import *
+from github import Github
+import nltk
+import math as m 
+# from math import cos
+
+var = m.cos(3.14)
+
+print(var)
